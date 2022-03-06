8d77a5e8d199a57c1ea56383cb9386f1de71ec8e
kischnerjennifer@gmail.com <kischnerjennifer@gmail.com>
2022-03-06 12:03:47 -0300

    Modifique Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,20 +4,19 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-
+import TheatersIcon from '@mui/icons-material/Theaters';
+import LocalActivityIcon from '@mui/icons-material/LocalActivity';
 
 const Nav = () =>{
     return (
-      <div>
-        
+              
         <Box sx={{ flexGrow: 1 }}>
           <AppBar 
           position="fixed"
-          sx={{ bgcolor: "red"}}>
+          sx={{ bgcolor: "#282828"}}>
             <Toolbar>
-              <IconButton
+              <LocalActivityIcon
                 size="large"
                 edge="start"
                 color="inherit"
@@ -25,36 +24,29 @@ const Nav = () =>{
                 sx={{ mr: 2 }}
               >
               <MenuIcon />
-              </IconButton>
+              </LocalActivityIcon>
               <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
                 Buscador Películas
               </Typography>
               <Button color="inherit">
-                <Link to="/"> Home </Link>
+                <Link to="/"  style={{ textDecoration: "none", color: "white" }}> Home </Link>
               </Button>
                  
               <Button>
-                <Link to="/ultimos-lanzamientos">Ultimos Lanzamientos</Link>
+                <Link to="/ultimos-lanzamientos"  style={{ textDecoration: "none", color: "white" }}>Ultimos Lanzamientos</Link>
               </Button> 
 
               <Button>
-                <Link to="/populares">Populares </Link>
+                <Link to="/populares"  style={{ textDecoration: "none", color: "white" }}>Populares </Link>
               </Button>
 
               <Button>
-              <Link to="/buscar">Buscar</Link>
+              <Link to="/buscar"  style={{ textDecoration: "none", color: "white" }}>Buscar</Link>
               </Button>
             </Toolbar>
           </AppBar>
         </Box>
         
-        <ul>
-            
-            <li></li>
-            <li> </li>
-        </ul>
-
-      </div>
   
     )
   }
